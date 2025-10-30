964721082c3e3ac548057e9cdecbfeccbc48fd90
unknown <b21827483@cs.hacettepe.edu.tr>
2025-10-30 14:54:14 +0300

    homepage and login, logout with auth0 added

---
+++ frontend/src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { BrowserRouter as Router } from 'react-router-dom'
+import AppRoutes from './AppRoutes.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <Router>
+      <AppRoutes />
+    </Router>
+  </StrictMode>
+)

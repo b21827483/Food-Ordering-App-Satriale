a6e86949f9ea5fe39a8a664777feb3a73ef74ac0
unknown <b21827483@cs.hacettepe.edu.tr>
2025-11-01 00:29:18 +0300

    get user func and pre-populate user-profile form added

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,8 +1,40 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { toast } from "sonner";
+import { type User } from "../types"
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+ 
+export const useGetMyUser = () => {
+    const { getAccessTokenSilently } = useAuth0();
+
+    const getMyUserRequest = async (): Promise<User> => {
+        const accessToken = getAccessTokenSilently();
+        const res = await fetch(`${API_BASE_URL}/api/my/user`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (!res.ok) {
+            throw new Error("Failed to fetch user");
+        }
+
+        return res.json();
+    };
+
+    const { data: currentUser, 
+            isLoading, 
+            error } = useQuery({queryKey: ["fetchCurrentUser"], queryFn: getMyUserRequest});
+
+    if (error) {
+        toast.error(error.toString());
+    }
+
+    return { currentUser, isLoading }; 
+};
 
 type CreateUserRequest = {
     auth0Id: string,
--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,10 +1,23 @@
-import { useUpdateMyUser } from "@/api/MyUserApi";
+import { useGetMyUser, useUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => {
-    const { updateUser, isPending } = useUpdateMyUser();
+    const { currentUser, isLoading: isGetLoading } = useGetMyUser();
+    const { updateUser, isPending: isUpdateLoading } = useUpdateMyUser();
+
+    if (isGetLoading) {
+        return <span>Loading...</span>
+    };
+
+    if (!currentUser) {
+        return <span>Unable to load user profile</span>
+    }
+
     return (
-        <UserProfileForm onSave={updateUser} isLoading={isPending} />
+        <UserProfileForm 
+          currentUser={currentUser}
+          onSave={updateUser}
+          isLoading={isUpdateLoading} />
     )
 };
 

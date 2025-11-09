c8e9da8d2c97d4f6bd27911231aa01532ae72ea7
unknown <b21827483@cs.hacettepe.edu.tr>
2025-11-09 23:40:21 +0300

    search request api added

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,8 +1,10 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 import { useParams } from "react-router-dom";
 
 const SearchPage = () => {
 
     const { city } = useParams();
+    const { results } = useSearchRestaurants(city);
 
     return (<span>{city}</span>)
 };

09afd6c8d00ec42e5bde5c4960397602ebfb4a3b
unknown <b21827483@cs.hacettepe.edu.tr>
2025-11-13 15:05:34 +0300

    restaurant detail page added

--- frontend/src/api/RestaurantApi.tsx
+++ frontend/src/api/RestaurantApi.tsx
@@ -1,8 +1,30 @@
 import type { SearchState } from "@/pages/SearchPage";
-import type { RestaurantSearchResponse } from "@/types";
+import type { Restaurant, RestaurantSearchResponse } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export const useGetRestaurant = (restaurantId?: string) => {
+    const getRestaurantByIdRequest = async (): Promise<Restaurant> => {
+
+        const res = await fetch(`${API_BASE_URL}/api/restaurant/${restaurantId}`);
+        
+        if (!res.ok) {
+            throw new Error("Failed to get restaurant");
+        }
+
+        return res.json();
+    };
+
+    const { data: restaurant, isLoading } = 
+        useQuery({
+            queryKey: ["fetchRestaurant"],
+            queryFn: getRestaurantByIdRequest,
+            enabled: !!restaurantId
+        });
+
+    return { restaurant, isLoading };
+};
 
 export const useSearchRestaurants = (searchState: SearchState, city?: string) => {
     const createSearchRequest = async (): Promise<RestaurantSearchResponse> => {
---
+++ frontend/src/pages/DetailPage.tsx
@@ -0,0 +1,24 @@
+import { useGetRestaurant } from "@/api/RestaurantApi";
+import { AspectRatio } from "@radix-ui/react-aspect-ratio";
+import { useParams } from "react-router-dom";
+
+const DetailPage = () => {
+
+    const { restaurantId } = useParams();
+    const { restaurant, isLoading } = useGetRestaurant();
+
+    if (isLoading || !restaurant) {
+        return "Loading..."
+    }
+
+    return (
+        <div className="flex flex-col gap-10">
+            <AspectRatio ratio={16/5}>
+                <img src={restaurant.imageUrl} className="rounded-md object-cover h-full w-full" />
+            </AspectRatio>
+        </div>
+    )
+};
+
+export default DetailPage
+

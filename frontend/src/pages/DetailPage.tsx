ef94acf5a6eaef8026bda1bc1775c0857afc5bd0
unknown <b21827483@cs.hacettepe.edu.tr>
2025-11-13 16:16:37 +0300

    menu items added to restaurant detail page

--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -20,7 +20,12 @@ const DetailPage = () => {
             <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32">
                 <div className="flex flex-col gap-4">
                     <RestaurantInfo restaurant={restaurant} />
-
+                    <span className="text-2xl font-bold tracking-tight">
+                        Menu
+                    </span>
+                    {restaurant.menuItems.map((menuItem) => (
+                        <MenuItem menuItem={menuItem} />
+                    ))}
                 </div>
             </div>
         </div>

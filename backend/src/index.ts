b9481ec20e7b4ab26fcfdf1c0028e03028dc2739
unknown <b21827483@cs.hacettepe.edu.tr>
2025-11-16 12:05:12 +0300

    create session api added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import { v2 as cloudinary } from "cloudinary";
 import myUserRoute from "./routes/MyUserRoute";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 
 mongoose
     .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -24,6 +25,7 @@ app.use(cors());
 app.use("/api/my/user", myUserRoute); 
 app.use("/api/my/restaurant", myRestaurantRoute);
 app.use("api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(7000, () => {
     console.log("server started on localhost:7000")

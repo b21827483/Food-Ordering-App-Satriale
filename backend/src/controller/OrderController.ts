9227abe643ae44e987d715adb2f3bdcf92a49957
unknown <b21827483@cs.hacettepe.edu.tr>
2025-11-18 23:56:13 +0300

    stripe CLI with nodemon added

--- backend/src/controller/OrderController.ts
+++ backend/src/controller/OrderController.ts
@@ -22,6 +22,13 @@ type CheckoutSessionRequest = {
     restaurantId: string;
 };
 
+const stripeWebhookHandler = async (req: Request, res: Response) => {
+    console.log("RECEIVED EVENT");
+    console.log("--------------");
+    console.log("event: ", req.body);
+    res.send()
+};
+
 const createCheckoutSession = async (req: Request, res: Response) => {
     try {
         const checkoutSessionRequest: CheckoutSessionRequest = req.body;
@@ -128,5 +135,6 @@ const createSession = async (
 };
 
 export default {
-    createCheckoutSession
+    createCheckoutSession,
+    stripeWebhookHandler,
 }
--- backend/src/routes/OrderRoute.ts
+++ backend/src/routes/OrderRoute.ts
@@ -11,4 +11,7 @@ router.post(
     OrderController.createCheckoutSession
 );
 
+router.post(
+    "/checkout/webhook", OrderController.stripeWebhookHandler);
+
 export default router
